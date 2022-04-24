a5fe5816bba5d743590202acccb3a8ff4811b8a5
Abhijit Kadalli <abhijitkadalli@nitgoa.ac.in>
2022-04-24 19:43:31 +0530

    Update PowerMethod.py

--- PowerMethod.py
+++ PowerMethod.py
@@ -1,11 +1,16 @@
 import numpy as numpy
+
 # Initialize the matrix
 A = numpy.array([[0.9,10.2,0.7],
                 [3.3,0.9,1.2],
                 [0.01,7.4,2.5]],float)
+
 # Initial Guess                
 a = numpy.array([[1],[1],[1]], float)
+
+
 n = int(input('Maximum Step: '))
+
 eigenvalue = 0
 for i in range(n):
     a = A@a
